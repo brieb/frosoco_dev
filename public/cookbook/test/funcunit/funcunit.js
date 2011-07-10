95af22c7d068e66a7136c06a598a8ce85c7863b5
Brie Bunge <bbunge@Brie-Bunges-MacBook-Pro.local>
2011-07-09 17:58:59 -0700

    Getting started with JSMVC - cookbook

---
+++ public/cookbook/test/funcunit/funcunit.js
@@ -0,0 +1,6 @@
+steal
+    .plugins("funcunit")
+    .then(
+    "cookbook_test",
+    "recipe_controller_test"
+);
---
+++ public/cookbook/test/qunit/qunit.js
@@ -0,0 +1,6 @@
+steal
+    .plugins("funcunit/qunit", "cookbook")
+    .then(
+    "cookbook_test",
+    "recipe_test"
+);
